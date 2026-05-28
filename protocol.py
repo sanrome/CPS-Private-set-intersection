29d044df598d0b68c4d57f9b41dada57093a730a
MinaMask <151130036+MinaMask@users.noreply.github.com>
2026-05-28 12:25:20 +0100

    Update protocol.py
    Fix: parameterize collusion threshold c, correct distribution loop, fix initial lambda

--- protocol.py
+++ protocol.py
@@ -1,16 +1,17 @@
 from polynomials import *
 from utils import *
 
-def run_protocol(players):
-    encrypted_fis = []
-    c = 2
+def run_protocol(players, c=1):
+    c = min(c, len(players) - 1)  # safety: c can't exceed n-1
     player_polynomials = [[] for _ in range(len(players))]
-    end_lambda = [0]
-    # each player sends its encrypted polynomial
+   # end_lambda = [0]
+    # each player sends its encrypted polynomial to the next c players in the ring
     for i in range(len(players)):
-        for j in range(1, c):
-            player_polynomials[(i + c) % len(players)].append(players[i].get_encrypted_polynomial())
-    
+        for j in range(1, c + 1):                          # range(1, c+1) = [1, 2, ..., c]
+             player_polynomials[(i + j) % len(players)].append(players[i].get_encrypted_polynomial())
+             
+    end_lambda = players[0].calculate_lambda(None, player_polynomials[0])
+
     for i in range(len(players)):
         end_lambda = players[i].calculate_lambda(end_lambda, player_polynomials[i])
 
