3ba4e00cbf6b970abc71c716e163444f6cd1d1ce
MinaMask <151130036+MinaMask@users.noreply.github.com>
2026-05-28 12:26:01 +0100

    Update player.py
    Fix: handle None previous_lambda in calculate_lambda

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from phe.command_line import encrypt
 from polynomials import polynomial_from_set, sample_random_polynomial
 from utils import *
+
 class Player:
     def __init__(self, player_id, private_set, public_key):
         self.id = player_id
@@ -17,6 +18,11 @@ class Player:
 
     def calculate_lambda(self, previous_lambda, encrypted_polynomials):
         phi = encrypted_multiply_plain(self.encrypted_polynomial, sample_random_polynomial(len(self.set)))
+        
         for p in encrypted_polynomials:
             phi = encrypted_add(phi, encrypted_multiply_plain(p, sample_random_polynomial(len(self.set))))
+
+        if previous_lambda is None:
+            return phi
+        
         return encrypted_add(previous_lambda, phi)
